3bfb8f7a5a2237319becb55396bb0a26ea3659d4
Dan Houghton <dan.houghton@gmail.com>
2025-11-07 06:28:18 +0000

    val_ prefix for validation metrics required

--- tiny_recursive_model/main.py
+++ tiny_recursive_model/main.py
@@ -5,7 +5,6 @@ from tiny_recursive_model.sudoku import SudokuDataModule
 
 # TODO:
 # - activation checkpointing to save memory (reduce_memory parameter)
-# - consider showing cell_acc and acc in validation logs
 # - reduce duplication in train_step and forward
 # - With 10% probability (halt_exploration_prob: 0.1), the model is forced to continue for a random number of steps (2 to 16)
 #   - min_halt_steps = (rand() < 0.1) * randint(2, 16)
@@ -15,7 +14,7 @@ from tiny_recursive_model.sudoku import SudokuDataModule
 #   - EMA - can use the Weighted callback built into Lightning (soon)
 #   - stablemax_cross_entropy loss function
 # - gpu utilisation: halted puzzles get replaced with fresh puzzles, so batch size is constant
-# - double check paper how it blends the losses
+# - paper has loss + 0.5 * (halt_loss  + continue_loss), so loss + 0.5 * halt_loss
 
 def main():
     LightningCLI(LitTRM, SudokuDataModule)
